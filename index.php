5117d70a37df8668b333d9afdb75ce29745e9f88
Lyralover228 <lyra2408@gmail.com>
2017-07-27 22:57:09 +0300

    Разделил S.P.E.C.I.A.L. на две колонки, поменял фон, поставил иконку

--- index.php
+++ index.php
@@ -2,12 +2,14 @@
 <html>
 	<head>
 		<title>Fallout input</title>
+		
 		<meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1">
 		<link href="css/bootstrap.css" rel="stylesheet">
 		<link rel="stylesheet" href="css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 		<script type="text/javascript" src="js/scripts.js"></script>
+		<link rel="shortcut icon" href="favicon.png"/>
 		<style>
-			body {margin:50px;}
+			body {margin:50px; color: #73ce81;}
 			    h3{
 					background-image: linear-gradient(to top, rgba(30,87,153,0), rgba(30,87,153,0.8), rgba(30,87,153,1),
                     rgba(30,87,153,1), rgba(41,137,216,1), rgba(30,87,153,1), rgba(30,87,153,1),
@@ -17,7 +19,7 @@
 		</style>
 	</head>
 
-	<body style="background-image:url(vaultboy.jpg)">
+	<body style="background-image:url(vaultboy.png)">
 		<form action="results.php" method="post">
 			<h3 class="text-center">Fallout character creation form</h3>
 			<div class="col-md-6">
@@ -43,19 +45,22 @@
 			
 			<div class="col-md-6">
 				<label>S.P.E.C.I.A.L. stats (stat range is 1 - 10; stat sum is 42):</label>
-				<br>
-				Strength: <input type=number class="form-control" id="1" name=strength value=5 style="width:70px;" required>
-				Perception: <input type=number class="form-control" id="2" name=perception value=5 style="width:70px;" required>
-				Endurance: <input type=number class="form-control" id="3" name=endurance value=5 style="width:70px;" required>
-				Charisma: <input type=number class="form-control" id="4" name=charisma value=5 style="width:70px;" required>
-				Intelligence: <input type=number class="form-control" id="5" name=intelligence value=5 style="width:70px;" required>
-				Agility: <input type=number class="form-control" name=agility id="6" value=5 style="width:70px;" required>
-				Luck: <input type=number  class="form-control" name=luck id="7" value=5 style="width:70px;" required><br>
-				<input type="button" class="btn btn-primary" value="Проверить остаток" onclick="addition(this.form)" onclick="is_full()"><br>
+				<div class="col-md-3"
+					<br>
+					Strength: <input type=number class="form-control" id="1" name=strength value=5 style="width:70px;" required>
+					Perception: <input type=number class="form-control" id="2" name=perception value=5 style="width:70px;" required>
+					Endurance: <input type=number class="form-control" id="3" name=endurance value=5 style="width:70px;" required>
+					Charisma: <input type=number class="form-control" id="4" name=charisma value=5 style="width:70px;" required>
+				</div>
+				<div class="col-md-3">
+					Intelligence: <input type=number class="form-control" id="5" name=intelligence value=5 style="width:70px;" required>
+					Agility: <input type=number class="form-control" name=agility id="6" value=5 style="width:70px;" required>
+					Luck: <input type=number  class="form-control" name=luck id="7" value=5 style="width:70px;" required><br>
+					<input type="button" class="btn btn-primary" value="Проверить остаток" onclick="addition(this.form)" onclick="is_full()"><br>
+				</div>
 				<h4>Остаток очков навыков: <span id='result'>7</span></h4>
 			</div>
-			
-			<input type=submit class="btn btn-primary" id="submit" disabled value="Confirm">
+			<input style="position: absolute; right: 10px; bottom: 15px;" type=submit class="btn btn-primary" id="submit" disabled value="Confirm">			
 		</form>
 	</body>
 </html>
